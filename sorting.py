d66a9f1ed086439cc45ee907358a77b3e97ea183
muriloWeber <caboeb@gmail.com>
2022-06-05 15:35:28 -0300

    mergeSort and test.py commit

---
+++ sorting.py
@@ -0,0 +1,27 @@
+# Algoritmo "Merge Sort" (recursivo, dividir para conquistar)
+
+def mergeSort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if(fim - inicio > 1):
+        meio = (fim + inicio)//2
+        mergeSort(lista, inicio, meio)
+        mergeSort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    top_left, top_right = 0, 0
+    for k in range(inicio, fim):
+        if top_left >= len(left):
+            lista[k] = right[top_right]
+            top_right += 1
+        elif top_right >= len(right):
+            lista[k] = left[top_left]
+        elif left[top_left] < right[top_right]:
+            lista[k] = left[top_left]
+            top_left += 1
+        else:
+            lista[k] = right[top_right]
+            top_right += 1
